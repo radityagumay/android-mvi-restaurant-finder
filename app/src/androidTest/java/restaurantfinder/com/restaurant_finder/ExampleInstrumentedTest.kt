2a291f89e3a796bd6e33f9eb28a486b0793de67e
radityagumay <raditya.g@go-jek.com>
2018-04-22 13:42:54 +0700

    first commit

---
+++ app/src/androidTest/java/restaurantfinder/com/restaurant_finder/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package restaurantfinder.com.restaurant_finder
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("restaurantfinder.com.restaurant_finder", appContext.packageName)
+    }
+}
