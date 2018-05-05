ebeae839093fe2325953c53f128279a7e239baa0
radityagumay <raditya.g@go-jek.com>
2018-05-06 00:50:33 +0700

    [Task] Add Retrofit dependency and module

--- app/src/main/java/com/rivuchk/restaurantfinder/external/di/module/HttpModule.kt
+++ app/src/main/java/com/rivuchk/restaurantfinder/external/di/module/HttpModule.kt
@@ -3,6 +3,7 @@ package com.rivuchk.restaurantfinder.external.di.module
 import com.rivuchk.restaurantfinder.data.network.NetworkService
 import dagger.Module
 import dagger.Provides
+import retrofit2.Retrofit
 
 @Module
 class HttpModule {
